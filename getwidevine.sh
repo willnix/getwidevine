5ebe81149336b13c46e366f34974b4d769b7c21f
Frieder Steinmetz <frieder.steinmetz@gmail.com>
2017-03-07 17:00:58 +0100

    Determine widevine version. Comments.

--- getwidevine.sh
+++ getwidevine.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/bash
+# download and unzip recovery for acer r13 (ARMv8)
 curl -s https://dl.google.com/dl/edgedl/chromeos/recovery/recovery.conf | grep -A 12 'Acer Chromebook R13' | grep -o 'https.*' | xargs curl | zcat > /tmp/image.bin
+
 mkdir /tmp/chromeos
+
+# mount the third partition of the image file
 MOUNTCMD=$(fdisk -l /tmp/image.bin 2> /dev/null | grep 'bin3 ' | awk '{print "mount -o loop,ro,offset="$2*512" /tmp/image.bin /tmp/chromeos"}')
 echo $MOUNTCMD
 $(sudo $MOUNTCMD)
+
+# get what we came for
 cp /tmp/chromeos/opt/google/chrome/libwidevinecdm*.so .
 cp /tmp/chromeos/opt/google/chrome/pepper/* .
+# get libwidevine version
+strings /tmp/chromeos/opt/google/chrome/chrome | grep -P '^\d\.\d\.\d\.\d\d\d$' > widevine.version
+
+# clean up
 sudo umount /tmp/chromeos
 rmdir /tmp/chromeos
 rm /tmp/image.bin
